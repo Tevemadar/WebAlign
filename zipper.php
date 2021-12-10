4c882c1e7988357817401e40b95c745ccb1bdfac
Gergely Csucs <Tevemadar@users.noreply.github.com>
2021-12-10 11:24:03 +0100

    Update zipper.php

--- zipper.php
+++ zipper.php
@@ -1,7 +1,8 @@
 <?php
 ob_start();
-strpos($_SERVER["HTTP_REFERER"],$_SERVER["REQUEST_SCHEME"]."://".$_SERVER["SERVER_NAME"]) === 0 and
-strpos($_SERVER["HTTP_REFERER"],str_replace("zipper.php", "webalign.html?", $_SERVER["REQUEST_URI"])) > 0 or die(print_r($_SERVER,true));
+$referer=$_SERVER["HTTP_REFERER"];
+$validator=$_SERVER["REQUEST_SCHEME"]."://".$_SERVER["SERVER_NAME"].str_replace("zipper.php", "webalign.html?", $_SERVER["REQUEST_URI"]);
+strpos($referer,$validator) === 0 or die($referer."\n".$validator);
 $zipfile=tempnam(sys_get_temp_dir(), "zip");
 if($_FILES["zip"]["size"]>0){
     move_uploaded_file($_FILES["zip"]["tmp_name"], $zipfile);
