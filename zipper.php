d073c79e78fce752a760d3a127164426c7d4476b
Gergely Csucs <Tevemadar@users.noreply.github.com>
2021-12-10 11:17:29 +0100

    origin-checker fix attempt

--- zipper.php
+++ zipper.php
@@ -1,7 +1,7 @@
 <?php
 ob_start();
-strpos($_SERVER["HTTP_REFERER"],
-       $_SERVER["REQUEST_SCHEME"]."://".$_SERVER["SERVER_NAME"].str_replace("zipper.php", "webalign.html?", $_SERVER["REQUEST_URI"])) === 0 or die("!");
+strpos($_SERVER["HTTP_REFERER"],$_SERVER["REQUEST_SCHEME"]."://".$_SERVER["SERVER_NAME"]) === 0 and
+strpos($_SERVER["HTTP_REFERER"],str_replace("zipper.php", "webalign.html?", $_SERVER["REQUEST_URI"])) > 0 or die(print_r($_SERVER,true));
 $zipfile=tempnam(sys_get_temp_dir(), "zip");
 if($_FILES["zip"]["size"]>0){
     move_uploaded_file($_FILES["zip"]["tmp_name"], $zipfile);
